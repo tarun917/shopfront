ff114da03c8ef49314518094d5c097cd94c95456
tarun917 <139982176+tarun917@users.noreply.github.com>
2024-06-15 11:53:07 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     <div className='nav'>
       <Link to='/' onClick={()=>{setMenu("shop")}} style={{ textDecoration: 'none' }} className="nav-logo">
         <img src={logo} alt="logo" />
-        <p>SHOPNOW</p>
+        <p>SHOPPER</p>
       </Link>
       <img onClick={dropdown_toggle} className='nav-dropdown' src={nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
